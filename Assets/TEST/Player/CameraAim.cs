cb35cc817f61c41e57e5c1d02dc806ba1796ce03
ekarlson238 <ekarlson238@gmail.com>
2020-03-17 18:10:08 -0500

    unity

---
+++ Assets/TEST/Player/CameraAim.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAim : MonoBehaviour
+{
+    [SerializeField]
+    private float aimSensitivity;
+
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private float yRotMin = -30, yRotMax = 60;
+
+    private float yRotationClamp = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        float rotationX = mouseX * aimSensitivity;
+        float rotationY = mouseY * aimSensitivity;
+
+        yRotationClamp += rotationY;
+        Debug.Log(yRotationClamp);
+
+        Vector3 rotation = this.transform.rotation.eulerAngles;
+        Vector3 playerRotation = player.transform.rotation.eulerAngles;
+
+        rotation.x -= rotationY;
+        rotation.y += rotationX;
+        playerRotation.y += rotationX;
+
+        if (yRotationClamp > yRotMax)
+        {
+            yRotationClamp = yRotMax;
+            rotation.x = -yRotMax;
+        }
+
+        if (yRotationClamp < yRotMin)
+        {
+            yRotationClamp = yRotMin;
+            rotation.x = -yRotMin;
+        }
+
+        player.transform.rotation = Quaternion.Euler(playerRotation);
+        this.transform.rotation = Quaternion.Euler(rotation);
+    }
+}
---
+++ Assets/TEST/Player/TestMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour
+{
+    public float speed = 500f;
+
+    private Rigidbody playerBody;
+
+    private float vertical;
+    private float horizontal;
+
+    public float jumpForce = 10;
+
+    private Vector3 velocity;
+
+    bool grounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    /// <summary>
+    /// The player moves by setting their rigidbody's velocity rather than using transform.translate or addForce
+    /// </summary>
+    private void Move()
+    {
+        Vector3 velocityX = playerBody.velocity;
+        Vector3 velocityY = playerBody.velocity;
+
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        
+        velocityY = Camera.main.transform.forward * vertical;
+        velocityX = Camera.main.transform.right * horizontal;
+
+        velocity = Vector3.Normalize(velocityX + velocityY);
+
+        velocity.y = playerBody.velocity.y / speed / Time.deltaTime;
+
+        playerBody.velocity = velocity * speed * Time.deltaTime;
+    }
+
+    private void Jump()
+    {
+        if (Input.GetAxis("Jump") != 0 && grounded)
+        {
+            playerBody.velocity = new Vector3(playerBody.velocity.x, jumpForce, playerBody.velocity.y);
+            grounded = false;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
+}
